4614cf2a85d3b2a1c05a8119b98d8d19a67cd7fa
Patricio Rearte <patriciorearte97@gmail.com>
2023-12-15 15:05:03 -0300

    creacion del navbar

---
+++ src/componentes/Formulario.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+const Formulario = () => {
+    const[ciudad,setCiudad]=useState("") // aca se guarda la ciudad buscada
+    
+    return ( 
+            <h1>hola</h1>
+     );
+}
+ 
+export default Formulario;
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar bg-dark text-light nb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Pronostico del clima a nivel mundial</h3>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
