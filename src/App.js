b6e6d23440e0c7d9f937407adc864b7258808881
Patricio Rearte <patriciorearte97@gmail.com>
2023-12-15 15:20:55 -0300

    input de busqueda terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './css/App.css';
 import NavBar from './componentes/NavBar';
+import Formulario from './componentes/Formulario';
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Formulario></Formulario>
     </div>
   );
 }
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,9 +1,20 @@
 import React,{useState} from 'react'
 const Formulario = () => {
     const[ciudad,setCiudad]=useState("") // aca se guarda la ciudad buscada
-    
+    const onSubmit =(e)=>{
+        e.preventDefault();
+        console.log(ciudad);
+    }
     return ( 
-            <h1>hola</h1>
+            <div className='container'>
+                <form onSubmit={onSubmit}>
+                    <div className='input-group mt-3 mx-auto' >
+                        <input type="text" className='form-control' placeholder='Ciudad' onChange={(e)=>setCiudad(e.target.value)} />
+                        <button className='btn btn-primary input-group-text' type='submit'>Buscar</button>
+                    </div>
+                </form>
+                
+            </div>
      );
 }
  
