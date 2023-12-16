968ea07792888bf7a10343296b872eca727765f3
Patricio Rearte <patriciorearte97@gmail.com>
2023-12-16 16:55:24 -0300

    realizacion de footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './css/App.css';
 import NavBar from './componentes/NavBar';
 import WeatherPanel from './componentes/WeatherPanel';
+import Footer from './componentes/Footer';
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <WeatherPanel></WeatherPanel>
+      <Footer></Footer>
     </div>
   );
 }
--- src/componentes/Form.jsx
+++ src/componentes/Form.jsx
@@ -14,7 +14,7 @@ const Form = ({newLocation}) => {
 
         <div className="container">
             <form onSubmit={onSubmit}>
-                <div className="input-group mb-3 mx-auto">
+                <div className="input-group mt-3 mx-auto">
                     <input type="text" className="form-control" placeholder="Ciudad" onChange={(e) =>setCity(e.target.value)}/>
                     <button className="btn btn-primary input-group-text" type="submit">Buscar</button>
                 </div>
